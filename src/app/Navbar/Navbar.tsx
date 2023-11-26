0abe5f0378872c309f83f8ed5350d1b1b83d11a2
Emmanuel Chijioke <manuelckay@gmail.com>
2023-11-26 04:27:42 +0100

    Changed Logo Case Sensitivity

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/assets/Logo.png";
+import Logo from "@/assets/Logo.png";
 import { redirect } from "next/navigation";
 import ShoppingCartButton from "./ShoppingCartButton";
 import { getCart  } from "../lib/db/cart";
@@ -27,7 +27,7 @@ export default async function Navbar () {
         <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
             <div className="flex-1">
                 <Link href="/" className="btn btn-ghost text-xl normal-case ">
-                    <Image src={logo} height={40} width={40} alt="Chefs Academy Logo" />
+                    <Image src={Logo} height={40} width={40} alt="Chefs Academy Logo" />
                     The Chefs Academy 
                 </Link>
             </div>
