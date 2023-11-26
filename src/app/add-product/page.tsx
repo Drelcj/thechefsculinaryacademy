6d58835517a342ed9034d51c1924634807a7736f
Emmanuel Chijioke <manuelckay@gmail.com>
2023-11-26 03:17:07 +0100

    first commit

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,81 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "../lib/db/prisma";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export const metadata = {
+  title: "Add Product - The Chefs' Shop",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signIn?callbackUrl=/add-product");
+  }
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);  
+
+  //throw Error("Bazinga!")
+
+  if (!name || !description || !imageUrl || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/"); 
+}
+
+export default async function AddProductPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/add-product");
+  }
+
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form action={addProduct}>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input input-bordered mb-3 w-full"
+        />
+        <FormSubmitButton className="btn-block">
+          Add Product
+        </FormSubmitButton>
+      </form>
+    </div>
+  );
+}
